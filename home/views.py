a8c3bf497c1514f826a60f77c55cb77dffffb943
majidnabilo <nabilo.majid@gmail.com>
2022-01-26 20:41:06 +0330

    create base.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
+
+
 
 
 def say_hello(request):
     person={'name':'majid','age':35}
-    return render(request,'index.html',context=person)
+    all = Todo.objects.all()
+    return render(request,'index.html',{'info':all})
 # Create your views here.
