4e572fcb43e86a50413017c127bcc5bf96435883
majidnabilo <nabilo.majid@gmail.com>
2022-01-26 21:33:36 +0330

    error detail 11

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home/', views.say_hello),
+    path('detail/<int:todo_id>/',views.detail)
 ]
--- home/views.py
+++ home/views.py
@@ -8,4 +8,7 @@ def say_hello(request):
     person={'name':'majid','age':35}
     all = Todo.objects.all()
     return render(request,'index.html',{'info':all})
+def detail(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    return render(request 'detail.html',{'todo': todo})
 # Create your views here.
