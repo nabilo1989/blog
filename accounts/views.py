2ea7779f3f63a18ddb9e3392a9cf99099e661f03
majidnabilo <nabilo.majid@gmail.com>
2022-01-27 19:06:52 +0330

    created app register

---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def user_register(request):
+    return HttpResponse('user registration')
+
+# Create your views here.
--- blog/urls.py
+++ blog/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
     path('', include('home.urls')),
+    path('accuonts/', include('accounts.urls')),
 
 ]
