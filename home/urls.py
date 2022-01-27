d7b39391334704602f10c83ef33ac084a38e4a00
majidnabilo <nabilo.majid@gmail.com>
2022-01-27 12:27:28 +0330

    created detai page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('home/', views.say_hello),
-    path('detail/<int:todo_id>/',views.detail)
+    path('detail/<int:todo_id>/', views.detail),
 ]
--- home/views.py
+++ home/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Todo
 
 
-
-
 def say_hello(request):
-    person={'name':'majid','age':35}
+    person = {'name': 'majid', 'age': 35}
     all = Todo.objects.all()
-    return render(request,'index.html',{'info':all})
-def detail(request,todo_id):
+    return render(request, 'index.html', {'info': all})
+
+
+def detail(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
-    return render(request 'detail.html',{'todo': todo})
+    return render(request, 'detail.html', {'todo': todo})
 # Create your views here.
